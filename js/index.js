3b2ecf7e2df0e0c2b2b123af6a4de41b14ff8bc9
Zachery Morris <ztm4qv@virginia.edu>
2020-04-09 19:28:38 -0400

    updated login page UI

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // Zachery Morris and Katharina Kemper
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-}
+// function signOut() {
+//     var auth2 = gapi.auth2.getAuthInstance();
+//     auth2.signOut().then(function () {
+//       console.log('User signed out.');
+//     });
+// }
